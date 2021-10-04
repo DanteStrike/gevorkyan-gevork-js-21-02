1ea0a653a6d710803a41a99a6cfd987bea943268
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-10-05 01:58:21 +0300

    COMPLETE task#3

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import * as taskOne from './tasks/task-1';
 import * as taskTwo from './tasks/task-2';
+import * as taskThree from './tasks/task-3';
 
 const tasks = document.querySelectorAll(`.task`);
 
 taskOne.init(tasks[0]);
 taskTwo.init(tasks[1]);
+taskThree.init(tasks[2]);
