3e5f09c46c4aa3b9d1cb1029849fd3a6cdf42bda
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-10-26 14:45:51 +0300

    ADD component Filter

---
+++ React_homework/13.intro/src/components/filter/Filter.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './Filter.css';
+
+
+export type FilterProps = {
+  title: string,
+  type: "checkbox" | "radio",
+  name: string,
+  items: Array<string>
+}
+
+const Filter = (
+  {
+    title,
+    type,
+    name,
+    items,
+  }: FilterProps
+) => {
+  return (
+    <fieldset className="filter">
+      <legend className="filter__title">{title}</legend>
+      <ul className="filter__items">
+        {items.map((item, index) =>
+          <li className="filter__item">
+            <input
+              className="filter__item-checkbox"
+              type={type}
+              id={`filter-${name}-${index}`}
+              value={index}
+              name={name}
+            />
+            <label className="filter__item-name" htmlFor={`filter-${name}-${index}`}>{item}</label>
+          </li>
+        )}
+      </ul>
+    </fieldset>
+  );
+}
+
+
+export {Filter};
