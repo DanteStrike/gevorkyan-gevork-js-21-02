6d28d83e6128aee9e5b794584ff98c616cf21f0d
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-10-26 14:45:43 +0300

    ADD component Advantages

---
+++ React_homework/13.intro/src/components/advantages/Advantages.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './Advantages.css';
+import {Advantage, AdvantageProps} from "../advantage/Advantage";
+
+
+type AdvantagesProps = {
+  advantages?: Array<AdvantageProps>
+}
+
+const Advantages = (
+  {
+    advantages = []
+  }: AdvantagesProps
+) => {
+  return (
+    <section className="advantages promo__advantages">
+      <h1 className="advantages__title">Рыба на любой вкус</h1>
+      <p className="advantages__tagline">Мы продаем рыбов, а не только показываем</p>
+      <div className="advantages__list">
+        {advantages.map((advantage) => <Advantage {...advantage}/>)}
+      </div>
+    </section>
+  );
+}
+
+
+export default Advantages;
