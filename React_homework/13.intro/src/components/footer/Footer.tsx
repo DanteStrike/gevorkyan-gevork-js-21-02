4bc77e447c28c4cfa71ae8f627a45309a801b582
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-10-26 14:46:18 +0300

    ADD component Footer

---
+++ React_homework/13.intro/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer promo-page__footer">
+      <a className="footer__contacts" href="#">Контакты</a>
+      <p className="footer__copyright">© 2021 ИП Рыбов О.А.</p>
+    </footer>
+  );
+}
+
+
+export default Footer;
