9f9593b5bd93dd231d1708fd24d088d41bda0b00
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-11-01 02:37:37 +0300

    ADD ToDo Filters Sort

--- React_homework/14.state_props/src/App.tsx
+++ React_homework/14.state_props/src/App.tsx
@@ -1,46 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import ToDo from "./components/todo/ToDo";
+import toDoListMock from "./mocks/todo-list";
+import {FilterType, SortType} from "./types";
 
-
-const mapStateToProps = {
-  nums: []
-}
-
-const mapDispToProps = {
-  add: (nums: number[]) => {
-    const arr = nums.slice();
-    arr.push(nums.length);
-    return arr;
-  },
-  del: (nums: number[]) => {
-    const arr = nums.slice();
-    arr.pop();
-    return arr;
-  }
-}
-
-
-function App() {
-  const [nums, setNum] = useState<number[]>([]);
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>Список</h1>
-      {nums.map((num, index) =>
-        <li key={index}>{num}</li>
-      )}
-      <button onClick={() => setNum(prevNums => {
-          const arr = prevNums.slice();
-          arr.push(prevNums.length);
-          return arr;
-        })
-      }>ADD</button>
-      <button onClick={() => setNum(prevNums => {
-        const arr = prevNums.slice();
-        arr.pop();
-        return arr;
-      })
-      }>DEL</button>
+    <div className="app">
+      <Header/>
+      <ToDo
+        initialFilter={FilterType.DEFAULT}
+        initialSort={SortType.DEFAULT}
+        value={toDoListMock}
+      />
+      <Footer/>
     </div>
   );
 }
