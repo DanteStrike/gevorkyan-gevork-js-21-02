a75984b31ad5051cf7c7a2816bd98adc3fa8516e
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-11-05 18:53:16 +0300

    ADD UsersList form

--- React_homework/17.router/src/App.tsx
+++ React_homework/17.router/src/App.tsx
@@ -1,66 +1,19 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import './App.css';
-import {IApi, IUserPreview} from './types';
-import Users from './components/users/Users';
-import Pagination from './components/pagination/Pagination';
-import Selector from './components/selector/Selector';
+import {IApi} from './types';
 import {ThemeContext} from './context/ThemeContext';
-import Switcher from './components/switcher/Switcher';
-import Loading from './components/loading/Loading';
+import UsersList from "./forms/users-list/UsersList";
 
 interface IAppProps {
   api: IApi;
 }
-interface IAppState {
-  isLoading: boolean;
-  users: IUserPreview[];
-  pageAmount: number;
-}
 
 function App({api}: IAppProps) {
-  const [loading, setLoading] = useState<IAppState>({
-    isLoading: false,
-    users: [],
-    pageAmount: 0,
-  });
-  const [page, setPage] = useState<number>(0);
-  const [limit, setLimit] = useState<number>(20);
-  const [limitOptions] = useState<number[]>([5, 10, 20]);
-
-  const handlePageChange = (value: number) => {
-    setPage(value);
-  };
-  const handleUsersPageLimitChange = (value: number) => {
-    setLimit(value);
-    setPage(0);
-  };
-  useEffect(() => {
-    setLoading((prevLoading) => ({...prevLoading, isLoading: true}));
-
-    api.getUsers(page, limit).then((list) =>
-      setLoading((prevLoading) => ({
-        ...prevLoading,
-        users: list.data,
-        pageAmount: Math.ceil(list.total / list.limit),
-        isLoading: false,
-      }))
-    );
-  }, [page, limit, api]);
-
-  const {users, pageAmount, isLoading} = loading;
   const {theme} = useContext(ThemeContext);
   return (
     <div className={`app app--theme_${theme}`}>
       <main className="app__container">
-        <h1 className="app__title">Пользователи</h1>
-        <Loading isLoading={isLoading}>
-          <Users users={users} />
-        </Loading>
-        <div className="app__wrap">
-          <Pagination value={pageAmount} defaultValue={page} onChange={handlePageChange} />
-          <Selector options={limitOptions} defaultOption={limit} onChange={handleUsersPageLimitChange} />
-          <Switcher />
-        </div>
+        <UsersList api={api}/>
       </main>
     </div>
   );
---
+++ React_homework/17.router/src/forms/users-list/UsersList.tsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react";
+import Loading from "../../components/loading/Loading";
+import Users from "../../components/users/Users";
+import Pagination from "../../components/pagination/Pagination";
+import Selector from "../../components/selector/Selector";
+import Switcher from "../../components/switcher/Switcher";
+import {IApi, IUserPreview} from "../../types";
+import "./UsersList.css";
+
+interface IUsersListProps {
+  api: IApi;
+}
+interface IUsersListState {
+  isLoading: boolean;
+  users: IUserPreview[];
+  pageAmount: number;
+}
+
+function UsersList({api}: IUsersListProps) {
+  const [loading, setLoading] = useState<IUsersListState>({
+    isLoading: false,
+    users: [],
+    pageAmount: 0,
+  });
+  const [page, setPage] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(20);
+  const [limitOptions] = useState<number[]>([5, 10, 20]);
+
+  const handlePageChange = (value: number) => {
+    setPage(value);
+  };
+  const handleUsersPageLimitChange = (value: number) => {
+    setLimit(value);
+    setPage(0);
+  };
+  useEffect(() => {
+    setLoading((prevLoading) => ({...prevLoading, isLoading: true}));
+
+    api.getUsers(page, limit).then((list) =>
+      setLoading((prevLoading) => ({
+        ...prevLoading,
+        users: list.data,
+        pageAmount: Math.ceil(list.total / list.limit),
+        isLoading: false,
+      }))
+    );
+  }, [page, limit, api]);
+
+  const {users, pageAmount, isLoading} = loading;
+
+  return (
+    <>
+      <h1 className="users-list__title">Пользователи</h1>
+      <Loading isLoading={isLoading}>
+        <Users users={users} />
+      </Loading>
+      <div className="users-list__wrap">
+        <Pagination value={pageAmount} defaultValue={page} onChange={handlePageChange} />
+        <Selector options={limitOptions} defaultOption={limit} onChange={handleUsersPageLimitChange} />
+        <Switcher />
+      </div>
+    </>
+  )
+}
+
+export default UsersList;
