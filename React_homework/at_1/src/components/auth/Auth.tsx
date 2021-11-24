8db3e045509eb9f04a49cb361f8c68211a12fed1
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-11-24 21:25:51 +0300

    ADD isAuth

--- React_homework/at_1/src/components/auth/Auth.tsx
+++ React_homework/at_1/src/components/auth/Auth.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Auth.scss';
+import {Avatar} from 'antd';
 
 function Auth() {
+  const [isAuth] = useState(true);
+
   return (
     <div className="auth">
-      <a className="auth__link auth__link--left" href="#е">
-        Вход
-      </a>
-      <a className="auth__link" href="#е">
-        Регистрация
-      </a>
+      {!isAuth ? (
+        <>
+          <a className="auth__link auth__link--left" href="#е">
+            Вход
+          </a>
+          <a className="auth__link" href="#е">
+            Регистрация
+          </a>
+        </>
+      ) : (
+        <>
+          <Avatar />
+          <a className="auth__link auth__link--left" href="#е">
+            Анжелика
+          </a>
+          <a className="auth__link auth__link--small" href="#е">
+            Выход
+          </a>
+        </>
+      )}
     </div>
   );
 }
