4e36a9dde999bb4d4481fdcd19395f927670ec62
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-11-25 02:18:39 +0300

    ADD UserCard

---
+++ React_homework/at_1/src/components/user-card/UserCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./UserCard.scss";
+
+function UserCard() {
+  return (
+    <article className="card">
+      <img className="card__img" alt="рисунок карточки" src="https://randomuser.me/api/portraits/men/3.jpg"/>
+      <h2 className="card__title">mr. Кавабанга</h2>
+    </article>
+  );
+};
+
+export default UserCard;
--- React_homework/at_1/src/forms/users/Users.tsx
+++ React_homework/at_1/src/forms/users/Users.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ContentLayout, {ContentLayoutType} from "../../components/content-layout/ContentLayout";
 import List from "../../components/list/List";
-import Card from "../../components/card/Card";
+import UserCard from "../../components/user-card/UserCard";
 
 function Users() {
   return (
     <ContentLayout type={ContentLayoutType.FULL} hideTitle title="Спиок слушателей">
       <List dataSource={[1, 2, 3, 4, 5, 6]} renderItem={((el) => (
         <List.Item key={el}>
-          <Card/>
+          <UserCard/>
         </List.Item>
       ))}/>
     </ContentLayout>
