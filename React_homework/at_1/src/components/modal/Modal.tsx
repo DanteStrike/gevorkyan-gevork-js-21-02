47475a06b5d817c43a42f9d49cb33a17ccb902f7
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-11-27 00:11:41 +0300

    ADD modal

---
+++ React_homework/at_1/src/components/modal/Modal.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Modal.scss";
+
+interface IModalProps {
+  isOpen: boolean
+  children: React.ReactNode
+  onClose: () => void
+}
+
+function Modal({children, isOpen, onClose}: IModalProps) {
+  const handleClose = () => {
+    onClose();
+  }
+
+  return (
+    <div className={`modal ${isOpen ? `` : `modal--hide`}`.trim()}>
+      <button type="button" className="modal__close" onClick={handleClose}>X</button>
+      <div className="modal__content">{children}</div>
+    </div>
+  );
+}
+
+export default Modal;
