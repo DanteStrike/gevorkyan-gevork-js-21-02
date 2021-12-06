4b4645f75f3d0ee1fb14b079720bb03de919f723
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-12-06 21:40:29 +0300

    ADD validate locale

--- React_homework/28.i18next/src/locale/en/error.ts
+++ React_homework/28.i18next/src/locale/en/error.ts
@@ -9,6 +9,35 @@ const ERROR = {
     notFoundProfile: `Profile not found`,
     notFoundPost: `Post not found`,
   },
+  validate: {
+    id: {
+      required: `Input ID`
+    },
+    age: {
+      required: `Input date of birth`,
+      border: `Only for {{age}}+`,
+    },
+    name: {
+      required: `Input name`,
+      min: `First name and last name must have over 2 characters`,
+      max: `First name and last name cant have over 50 characters`
+    },
+    gender: {
+      required: `Input Gender`
+    },
+    avatar: {
+      imgFormat: `Only PNG and JPEG`,
+      length: `Avatar img size < 2Mb`
+    },
+    email: {
+      required: `Input email`,
+      format: `Input correct email`,
+    },
+    phone: {
+      required: `Input phone`,
+      format: `Input correct phone`
+    }
+  }
 };
 
 export default ERROR;
--- React_homework/28.i18next/src/utils/validate.ts
+++ React_homework/28.i18next/src/utils/validate.ts
@@ -1,27 +1,28 @@
 import {RuleObject} from 'antd/es/form';
 import moment from 'moment';
 import {message, Upload} from 'antd';
+import i18next from '../locale/i18next';
 import DataUtils from './data';
 
 const requireValidator = (text: string) => () => ({
   validator(_: RuleObject, value: any) {
     if (!value) {
-      return Promise.reject(new Error(text));
+      return Promise.reject(new Error(i18next.t(text)));
     }
     return Promise.resolve();
   },
 });
-const requireValidatorID = requireValidator(`Укажите ID`);
-const requireValidatorName = requireValidator(`Укажите ФИО`);
-const requireValidatorGender = requireValidator(`Укажите ПОЛ`);
-const requireValidatorBirth = requireValidator(`Укажите дату рождения`);
-const requireValidatorEmail = requireValidator(`Укажите почту`);
-const requireValidatorPhone = requireValidator(`Укажите телефон`);
+const requireValidatorID = requireValidator(`error.validate.id.required`);
+const requireValidatorName = requireValidator(`error.validate.name.required`);
+const requireValidatorGender = requireValidator(`error.validate.gender.required`);
+const requireValidatorBirth = requireValidator(`error.validate.age.required`);
+const requireValidatorEmail = requireValidator(`error.validate.email.required`);
+const requireValidatorPhone = requireValidator(`error.validate.phone.required`);
 
 const yearOldValidator = (age: number) => () => ({
   validator(_: RuleObject, value: any) {
     if (value > moment().subtract(age, `years`)) {
-      return Promise.reject(new Error(`Доступ только для ${age}+`));
+      return Promise.reject(new Error(i18next.t(`error.validate.age.border`, {age})));
     }
     return Promise.resolve();
   },
@@ -33,11 +34,11 @@ const userNameValidator = () => ({
     const {firstName, lastName} = DataUtils.normalizeName(value);
 
     if (firstName.length < 2 || lastName.length < 2) {
-      return Promise.reject(new Error('Имя и фамилия должны содержать больше 2-х символов'));
+      return Promise.reject(new Error(i18next.t(`error.validate.name.min`)));
     }
 
     if (firstName.length > 50 || lastName.length > 50) {
-      return Promise.reject(new Error('Имя и фамилия не могут быть длинее 50-и символов'));
+      return Promise.reject(new Error(i18next.t(`error.validate.name.max`)));
     }
 
     return Promise.resolve();
@@ -47,17 +48,17 @@ const userNameValidator = () => ({
 const imageValidator = (file: File) => {
   const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
   if (!isJpgOrPng) {
-    message.error('Можно использовать только PNG и JPEG');
+    message.error(i18next.t(`error.validate.avatar.imgFormat`));
   }
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
-    message.error('Аватарка должна весить < 2Mb');
+    message.error(i18next.t(`error.validate.avatar.length`));
   }
   return (isJpgOrPng && isLt2M) || Upload.LIST_IGNORE;
 };
 
-const phoneFormatValidator = {pattern: new RegExp(`^\\+?7(\\d{10})$`), message: 'Введите корректный телефон'};
-const emailFormatValidator = {type: 'email' as any, message: 'Введите корректную почту'};
+const phoneFormatValidator = () => ({pattern: new RegExp(`^\\+?7(\\d{10})$`), message: i18next.t(`error.validate.phone.format`)});
+const emailFormatValidator = () => ({type: 'email' as any, message: i18next.t(`error.validate.email.format`)});
 const disableDateOverCurrent = (current: moment.Moment) => current && current > moment();
 
 export default {
