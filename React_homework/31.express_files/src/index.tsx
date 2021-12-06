0f448514b925de121d4f8e95dfcee449efc11e60
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-12-06 22:02:37 +0300

    Init env

---
+++ React_homework/31.express_files/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+/* eslint-disable import/no-extraneous-dependencies */
+import 'antd/dist/antd.min.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
