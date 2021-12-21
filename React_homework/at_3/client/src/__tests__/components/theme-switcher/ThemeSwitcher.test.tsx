2b683094f7e7dfb52a793e0c652a2411914613e0
Gevork Gevorkyan <bmstu.gg@gmail.com>
2021-12-21 19:20:41 +0300

    Format tests

--- React_homework/at_3/client/src/__tests__/components/theme-switcher/ThemeSwitcher.test.tsx
+++ React_homework/at_3/client/src/__tests__/components/theme-switcher/ThemeSwitcher.test.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import {mount, shallow} from "enzyme";
-import {Theme, ThemeContext} from "../../../context/ThemeContext";
-import ThemeSwitcher from "../../../components/theme-switcher/ThemeSwitcher";
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {Theme, ThemeContext} from '../../../context/ThemeContext';
+import ThemeSwitcher from '../../../components/theme-switcher/ThemeSwitcher';
 
 describe(`Component ThemeSwitcher should render correctly`, () => {
   it(`snapshot default test`, () => {
-    const wrapper = shallow(<ThemeSwitcher/>)
+    const wrapper = shallow(<ThemeSwitcher />);
 
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+});
 
 describe(`Component ThemeSwitcher should work correctly`, () => {
   it(`snapshot default test`, () => {
     const mockContext: any = {
       theme: Theme.LIGHT,
       setTheme: jest.fn((val) => {
-        mockContext.theme = val
-      })
-    }
+        mockContext.theme = val;
+      }),
+    };
 
     const wrapper = mount(
       <ThemeContext.Provider value={mockContext}>
-        <ThemeSwitcher/>
+        <ThemeSwitcher />
       </ThemeContext.Provider>
-    )
+    );
     const component = wrapper.find(`Switch`).at(0);
     expect(component.prop(`checked`)).toBe(false);
     component.simulate(`click`, true);
@@ -32,9 +32,9 @@ describe(`Component ThemeSwitcher should work correctly`, () => {
 
     const wrapperDark = mount(
       <ThemeContext.Provider value={mockContext}>
-        <ThemeSwitcher/>
+        <ThemeSwitcher />
       </ThemeContext.Provider>
-    )
+    );
     const componentDark = wrapperDark.find(`Switch`).at(0);
     expect(componentDark.prop(`checked`)).toBe(true);
     componentDark.simulate(`click`, false);
@@ -42,10 +42,10 @@ describe(`Component ThemeSwitcher should work correctly`, () => {
 
     const wrapperLight = mount(
       <ThemeContext.Provider value={mockContext}>
-        <ThemeSwitcher/>
+        <ThemeSwitcher />
       </ThemeContext.Provider>
-    )
+    );
     const componentLight = wrapperLight.find(`Switch`).at(0);
     expect(componentLight.prop(`checked`)).toBe(false);
-  })
-})
+  });
+});
--- React_homework/at_3/client/src/__tests__/components/user-preview/UserPreview.test.tsx
+++ React_homework/at_3/client/src/__tests__/components/user-preview/UserPreview.test.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import {shallow} from "enzyme";
-import UserPreview from "../../../components/user-preview/UserPreview";
-import {fullUser} from "../../mocks/user.mock";
+import {shallow} from 'enzyme';
+import UserPreview from '../../../components/user-preview/UserPreview';
+import {fullUser} from '../../mocks/user.mock';
 
 describe(`Component UserPreview should render correctly`, () => {
   it(`snapshot default test`, () => {
-    const wrapper = shallow(
-      <UserPreview
-        isUser={false}
-        user={fullUser}
-        isLoading={false}
-      />
-    )
+    const wrapper = shallow(<UserPreview isUser={false} user={fullUser} isLoading={false} />);
 
     expect(wrapper.find(`.user-preview__edit`)).toHaveLength(0);
     expect(wrapper).toMatchSnapshot();
   });
 
   it(`snapshot auth user test`, () => {
-    const wrapper = shallow(
-      <UserPreview
-        isUser
-        user={fullUser}
-        isLoading={false}
-      />
-    )
+    const wrapper = shallow(<UserPreview isUser user={fullUser} isLoading={false} />);
 
     expect(wrapper.find(`.user-preview__edit`)).toHaveLength(1);
     expect(wrapper).toMatchSnapshot();
@@ -33,16 +21,10 @@ describe(`Component UserPreview should render correctly`, () => {
 
 describe(`Component UserPreview should work correctly`, () => {
   it(`loading test`, () => {
-    const wrapper = shallow(
-      <UserPreview
-        isUser={false}
-        user={fullUser}
-        isLoading
-      />
-    )
+    const wrapper = shallow(<UserPreview isUser={false} user={fullUser} isLoading />);
 
     expect(wrapper.find(`Memo(Loading)`).prop(`isLoading`)).toBe(true);
     wrapper.setProps({isLoading: false});
     expect(wrapper.find(`Memo(Loading)`).prop(`isLoading`)).toBe(false);
   });
-})
+});
